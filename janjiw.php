d6f60786b628b5621458fc268fea4c464491a6c5
janurendra05 <57127449+janurendra05@users.noreply.github.com>
2020-09-17 09:13:19 +0700

    Update janjiw.php

--- janjiw.php
+++ janjiw.php
@@ -4,7 +4,7 @@ $headers = array();
 $headers[] = 'Content-Type: application/json; charset=utf-8';
 $headers[] = 'User-Agent: okhttp/3.12.1';
 $headers[] = 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjhjODFlM2I2MGUzNjkxNDgzMzc2NGFkZmQ3Njk5MGRhOGNlN2ZkNTdiNDk0ZTYyYjQzN2IwNmZiOTgwMzRkMGVlODFlOWQ4N2JhMjQyNzEzIn0.eyJhdWQiOiIxIiwianRpIjoiOGM4MWUzYjYwZTM2OTE0ODMzNzY0YWRmZDc2OTkwZGE4Y2U3ZmQ1N2I0OTRlNjJiNDM3YjA2ZmI5ODAzNGQwZWU4MWU5ZDg3YmEyNDI3MTMiLCJpYXQiOjE2MDAyMzU1NjQsIm5iZiI6MTYwMDIzNTU2NCwiZXhwIjoyODk2MjM1NTYzLCJzdWIiOiIiLCJzY29wZXMiOlsiYXBwcyJdfQ.LG-jNO64pYEQrnh57AcvTfQYmZ7E7tf-9NYVUYOJtQIWFV0s6jivhuR6i5QHZL40nxLo52Iplx24mgDhdWNhSn9v5YAujMHidPAuiXZo9MUQml9EhpfqzH8vVthd3FfouELmo4NgnSiNLOwhS9PyRV6S29soOpW_hIelFZxEU6sWelRZRBaN2P7aYtU9mqi0sfL6vVNAxaXhQgS52MLYWUxAm5U6IjLC2GCt_7ep-hwl-Ml0vzOfjPdof_caylT6IH2qDAWdaPXIqadZcOrthqguCS4LqKD5wjcgUK2C9XivC5PPGNQEiLd_lK5vxSAk03GFl65p_RJXdPGgf6htdzitZ7Q9cXHpQZrv_Bgf1xHOIkJq9npU1guwpMRsYAWNLFDjwUk0fokjyJH3bRf_7ASDS1sp8aCjWtdn2r7-yCKoEGC_NmLQvjaNlB_gnkkcQck2FeJLURAaDjVJAVEFjbz0AwH6KSKlmgfo68K3bpthBjykZiCTov89cW-jnZIdlD4THyIphdaWLiVX9hwR6ZqOI3rmCLasTnZ5nqlTXkhPD4YB-LfaTBROvXLHe2cl6WIj9QXA5n4OsHXszp1RYkOf98bJXhO76LusHv_viOQum7M2PIxf-IJqZHKJNPFb8QZ55v5LVqrkZMndyZGX_iH2GJSL-sucsRKR_p5_a6g';
-
+Ulang:
 $nope = '0812'.random(8,0);
 $dev = random(16,1);
 $na = explode(" ", nama());
@@ -26,14 +26,21 @@ if (strpos($cek[1], 'Anda akan mendaftar')) {
 				Token:
 				$tkn = curl('https://api.jiwa.app/oauth/token', '{"client_id":2,"client_secret":"DhS2WkfjVV8sdRJOhJra84qFauISRSeMsV0G4fkV","grant_type":"password","password":"121212","scope":"apps","username":"'.$nope.'"}', $headers);
 				if (strpos($tkn[1], '"access_token"')) {
-					$cfg = curl('https://api.jiwa.app/api/users/profile/update', '{"address":"","birthday":"","celebrate":"","email":"'.strtolower(trim($na[0])).mt_rand(10,99).'@v7ecub.com","gender":"","id_city":"","job":"","name":"'.trim($na[0]).'","phone":"'.$nope.'","pin":"'.$pin.'"}', $headers);
+					$cfg = curl('https://api.jiwa.app/api/users/profile/update', '{"address":"","birthday":"","celebrate":"","email":"'.strtolower(trim($na[0])).mt_rand(10,9999).'@v7ecub.com","gender":"","id_city":"","job":"","name":"'.trim($na[0]).'","phone":"'.$nope.'","pin":"'.$pin.'"}', $headers);
 					if (strpos($cfg[1], '"status":"success"')) {
 						$token = json_decode($tkn[1])->access_token;
 						array_pop($headers);
 						$headers[] = 'Authorization: Bearer '.$token;
 						$gas = curl('https://api.jiwa.app/api/home/membership', '{"device_token":"c7lBDLiAT_uACerrBiBT65:APA91bE7OguwPwspE4TwK_z6fVv_4sNxWD9X_b4jBa1-wRN_PunIVCQIyoFpSdoHWzaMzCauXLk4Qp7ZqbnHiAWn7zP7BRs1zHRHIyZWlBBFdrEvztp0ZGqvG4ZL'.random(16,1).'","device_id":"'.$dev.'","device_type":"Android"}', $headers);
 						if (strpos($gas[1], '"status":"success"')) {
-							echo "[+] Registration Successfuly!\n";		
+							echo "[+] Registration Successfuly!\n";
+
+							$live = "akun.txt";
+							$fopen1 = fopen($live, "a+");
+							$fwrite1 = fwrite($fopen1,$nope. "\n");
+							goto Ulang;
+
+
 						} else {
 							die($gas[1]);
 						}
@@ -69,7 +76,7 @@ function nama()
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		$ex = curl_exec($ch);
 		preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
-		return $name[2][mt_rand(0, 14) ];
+		return $name[2][mt_rand(0,9) ];
 	}
 
 function curl($url,$post,$headers,$follow=false,$method=null)
